5e48f572e745c7a3d59ce6d60184918d88680922
luanpoppe <luanpoppe@gmail.com>
2024-12-02 20:22:24 -0300

    feat: adicionando props

--- src/index.js
+++ src/index.js
@@ -2,38 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const books = [
+  {
+    author: "Alice Walstead",
+    title: "How to Catch a Reindeer",
+    img: "./images/book-1.jpg",
+    id: 1,
+  },
+  {
+    author: "Sarah J. Maas",
+    title: "A Court of Thorns and Roses",
+    img: "https://m.media-amazon.com/images/I/81RrEEMiOCL._SL1500_.jpg",
+    id: 2,
+  },
+];
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <EventExamples />
+      {books.map((book) => (
+        <Book key={book.id} {...book} />
+      ))}
     </section>
   );
 };
 
-const Book = () => {
+const EventExamples = () => {
   return (
-    <article className="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input style={{ margin: "1rem 0" }} type="text" name="example" />
+      </form>
+      <button>Click Here</button>
+    </section>
   );
 };
 
-const Image = () => (
-  <img src="./images/book-1.jpg" alt="How to Catch a Reindeer" />
-);
-const Title = () => <h2>How to Catch a Reindeer</h2>;
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-  return <h4 style={inlineHeadingStyles}>Alice Walstead</h4>;
+const Book = ({ author, img, title }) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
