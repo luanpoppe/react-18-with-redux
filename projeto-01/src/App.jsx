2d794e716081c4e7392c6ca292fbfaff05af80f5
luanpoppe <luanpoppe@gmail.com>
2024-12-11 20:16:15 -0300

    feat: finalizado módulo 06

--- projeto-01/src/App.jsx
+++ projeto-01/src/App.jsx
@@ -1,13 +1,9 @@
-import Start from "./tutorial/04-project-structure/starter/Navbar";
-// import { Navbar } from "./tutorial/04-project-structure/starter/Navbar/Navbar";
-import { List } from "./tutorial/05-leverage-javascript/starter/List";
+import Start from "./tutorial/06-forms/starter/05-form-data";
 
 function App() {
   return (
     <div className="container">
-      {/* <Start /> */}
-      {/* <Navbar /> */}
-      <List />
+      <Start />
     </div>
   );
 }
--- projeto-01/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ projeto-01/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,61 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UserChallenge = () => {
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState(data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) return;
+
+    const fakeId = Date.now();
+    const newUser = { id: fakeId, name: name };
+    const updatedUsers = [...users, newUser];
+    setUsers(updatedUsers);
+    setName("");
+
+    console.log("form submitted");
+  };
+
+  const removeUser = (id) => {
+    const updatedUsers = users.filter((person) => {
+      return person.id !== id;
+    });
+    setUsers(updatedUsers);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       {/* render users below */}
+      <h4>users</h4>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h4>{user.name}</h4>
+          <button onClick={() => removeUser(user.id)} className="btn">
+            remove
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
