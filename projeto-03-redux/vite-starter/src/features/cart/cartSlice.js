37bce43b6fea1a9ab4ca410761710bc6693b3cc7
luanpoppe <luanpoppe@gmail.com>
2024-12-14 12:31:01 -0300

    feat: finalização do curso

--- projeto-03-redux/vite-starter/src/features/cart/cartSlice.js
+++ projeto-03-redux/vite-starter/src/features/cart/cartSlice.js
@@ -1,5 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
 
 const initialState = {
   cartItems: cartItems,
@@ -7,6 +9,18 @@ const initialState = {
   total: 0,
   isLoading: true,
 };
+
+export const getCartItems = createAsyncThunk("cart/getCartItems", async () => {
+  try {
+    const response = await resp.json();
+    return response;
+  } catch (error) {
+    console.log("error: ", error);
+  }
+  // return fetch(url)
+  //   .then((resp) => resp.json())
+  //   .catch((err) => console.log(err));
+});
 
 const cartSlice = createSlice({
   name: "cart",
@@ -41,6 +55,31 @@ const cartSlice = createSlice({
       state.amount = amount;
       state.total = total;
     },
+  },
+  // extraReducers: {
+  //   [getCartItems.pending]: (state) => {
+  //     state.isLoading = true;
+  //   },
+  //   [getCartItems.fulfilled]: (state, action) => {
+  //     state.isLoading = false;
+  //     state.cartItems = action.payload;
+  //   },
+  //   [getCartItems.rejected]: (state) => {
+  //     state.isLoading = true;
+  //   },
+  // },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.cartItems = action.payload;
+      })
+      .addCase(getCartItems.rejected, (state) => {
+        state.isLoading = true;
+      });
   },
 });
 
